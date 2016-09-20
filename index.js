a79f8ac456131fd7a7270959d0b65a30cc521ea6
Mark Waldron <markewaldron@gmail.com>
2016-09-20 17:32:15 -0400

    Added redis rate limiter to auth controller

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
-var	ControllerSettings = require('./model/controller_settings.js');
-var	clientSchema = require('./model/client.js');
-var	codeSchema = require('./model/code.js');
-var	tokenSchema = require('./model/token.js');
+var ControllerSettings = require('./model/controller_settings.js');
+var clientSchema = require('./model/client.js');
+var codeSchema = require('./model/code.js');
+var tokenSchema = require('./model/token.js');
+var RateLimit = require('express-rate-limit');
+var RedisStore = require('rate-limit-redis');
+var redis = require('redis');
 var fs = require('fs-extra');
 var extend = require('utils-merge');
 var path = require('path');
@@ -10,7 +13,7 @@ var oauth2serverExtSettings;
 var appenvironment;
 var settingJSON;
 var oauth2serverExtSettingsFile = path.join(__dirname, '../../content/config/extensions/periodicjs.ext.oauth2server/settings.json');
-var	defaultExtSettings = require('./controller/default_config');
+var defaultExtSettings = require('./controller/default_config');
 /**
  * An asset upload manager that uses pkgcloud to upload to the various cloud service providers (amazon s3, rackspace cloud files
  * @{@link https://github.com/typesettin/periodicjs.ext.clouduploads}
@@ -22,68 +25,67 @@ var	defaultExtSettings = require('./controller/default_config');
  * @param  {object} periodic variable injection of resources from current periodic instance
  */
 module.exports = function (periodic) {
-	//register mongo models
-	var Client = periodic.mongoose.model('Client', clientSchema);
-	var Code = periodic.mongoose.model('Code', codeSchema);
-	var Token = periodic.mongoose.model('Token', tokenSchema);
+  //register mongo models
+  var Client = periodic.mongoose.model('Client', clientSchema);
+  var Code = periodic.mongoose.model('Code', codeSchema);
+  var Token = periodic.mongoose.model('Token', tokenSchema);
 
-	appenvironment = periodic.settings.application.environment;
-	settingJSON = fs.readJsonSync(oauth2serverExtSettingsFile,{throws:false});
-	//console.log('before settingJSON[appenvironment]', settingJSON[appenvironment]);
-	oauth2serverExtSettings = (settingJSON && settingJSON[appenvironment]) ? extend(defaultExtSettings, settingJSON[appenvironment]) : defaultExtSettings;
+  appenvironment = periodic.settings.application.environment;
+  settingJSON = fs.readJsonSync(oauth2serverExtSettingsFile, { throws: false });
+  //console.log('before settingJSON[appenvironment]', settingJSON[appenvironment]);
+  oauth2serverExtSettings = (settingJSON && settingJSON[appenvironment]) ? extend(defaultExtSettings, settingJSON[appenvironment]) : defaultExtSettings;
 
 
-	periodic.app.controller.extension.oauth2server = {
-		client:periodic.core.controller.controller_routes(ControllerSettings.client)
-	};
-	periodic.app.controller.extension.oauth2server.settings = oauth2serverExtSettings;
-	periodic.app.controller.extension.oauth2server.auth = require('./controller/auth.js')(periodic);
-	periodic.app.controller.extension.oauth2server.server = require('./controller/oauth2.js')(periodic);
-	var clientRouter = periodic.express.Router();
-	var apiRouter = periodic.express.Router();
-	var clientController = periodic.app.controller.extension.oauth2server.client;
-	var oauth2authController = periodic.app.controller.extension.oauth2server.auth;
-	var oauth2serverController = periodic.app.controller.extension.oauth2server.server;
-	var authController = periodic.app.controller.extension.login.auth;
-	var uacController = periodic.app.controller.extension.user_access_control.uac;
-	var userroleController = periodic.app.controller.native.userrole;
+  periodic.app.controller.extension.oauth2server = {
+    client: periodic.core.controller.controller_routes(ControllerSettings.client)
+  };
+  periodic.app.controller.extension.oauth2server.settings = oauth2serverExtSettings;
+  periodic.app.controller.extension.oauth2server.auth = require('./controller/auth.js')(periodic);
+  periodic.app.controller.extension.oauth2server.server = require('./controller/oauth2.js')(periodic);
+  var clientRouter = periodic.express.Router();
+  var apiRouter = periodic.express.Router();
+  var clientController = periodic.app.controller.extension.oauth2server.client;
+  var oauth2authController = periodic.app.controller.extension.oauth2server.auth;
+  var oauth2serverController = periodic.app.controller.extension.oauth2server.server;
+  var authController = periodic.app.controller.extension.login.auth;
+  var uacController = periodic.app.controller.extension.user_access_control.uac;
+  var userroleController = periodic.app.controller.native.userrole;
 
-	// express,app,logger,config,db,mongoose
-	// 		assetController = periodic.app.controller.native.asset,
+  clientRouter.get('/',
+    clientController.loadClientsWithDefaultLimit,
+    clientController.loadClientsWithCount,
+    clientController.loadClients,
+    clientController.index);
+  clientRouter.get('/:id',
+    clientController.loadClient,
+    clientController.show);
+  clientRouter.post('/new', oauth2authController.set_client_data, clientController.create);
+  
+  apiRouter.get('/jwt/token', oauth2authController.get_jwt_token);
 
-	clientRouter.get('/', 
-		clientController.loadClientsWithDefaultLimit, 
-		clientController.loadClientsWithCount, 
-		clientController.loadClients, 
-		clientController.index);
-	clientRouter.get('/:id', 
-		clientController.loadClient, 
-		clientController.show);
-	clientRouter.post('/new',oauth2authController.set_client_data,clientController.create);
+  // Create endpoint handlers for oauth2 authorize
+  apiRouter.route('/oauth2/authorize')
+    .get(authController.ensureAuthenticated,
+    uacController.loadUserRoles,
+    uacController.check_user_access,
+    oauth2serverController.authorization)
+    .post(authController.ensureAuthenticated,
+    oauth2serverController.decision);
 
-	apiRouter.get('/jwt/token',oauth2authController.get_jwt_token);
+  apiRouter.get('/oauth2/profile',
+    oauth2authController.ensureApiAuthenticated,
+    oauth2authController.get_user_profile);
 
-	// Create endpoint handlers for oauth2 authorize
-	apiRouter.route('/oauth2/authorize')
-	  .get(authController.ensureAuthenticated,
-	  	uacController.loadUserRoles, 
-	  	uacController.check_user_access, 
-	  	oauth2serverController.authorization)
-	  .post(authController.ensureAuthenticated,
-	  	oauth2serverController.decision);
+  // Create endpoint handlers for oauth2 token
+  apiRouter.route('/oauth2/token')
+    .post(oauth2authController.isClientAuthenticated, oauth2serverController.token);
 
-	 apiRouter.get('/oauth2/profile',oauth2authController.ensureApiAuthenticated, oauth2authController.get_user_profile);
+  //get jwt auth token
+  apiRouter.get('/jwt/profile', oauth2authController.isJWTAuthenticated, oauth2authController.get_user_profile);
 
-	// Create endpoint handlers for oauth2 token
-	apiRouter.route('/oauth2/token')
-	  .post(oauth2authController.isClientAuthenticated, oauth2serverController.token);
+  // Register all our routes with /api
+  periodic.app.use('/api', apiRouter);
 
-	//get jwt auth token
-	apiRouter.get('/jwt/profile',oauth2authController.isJWTAuthenticated, oauth2authController.get_user_profile);
-
-	// Register all our routes with /api
-	periodic.app.use('/api', apiRouter);
-
-	periodic.app.use('/' + periodic.app.locals.adminPath + '/oauth/client', clientRouter);
-	return periodic;
+  periodic.app.use('/' + periodic.app.locals.adminPath + '/oauth/client', clientRouter);
+  return periodic;
 };
--- model/client.js
+++ model/client.js
@@ -2,99 +2,113 @@
 
 const crypto = require('crypto');
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-	ObjectId = Schema.ObjectId,
-	logger = console;
+  Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId,
+  logger = console;
 
 var clientSchema = new Schema({
-	id: ObjectId,
-	createdat: {
-		type: Date,
-		'default': Date.now
-	},
-	updatedat: {
-		type: Date,
-		'default': Date.now
-	},
-	name: {
-		type: String,
-		required: true,
-		index: {
-			unique: true
-		}
-	},
-	title: String,
-	user_id: {
-		type: ObjectId,
-		ref: 'User'
-	},
-	user_entity_type: {
-		type: String,
-		'default': 'user'
-	},
-	client_id: {
-		type: String,
-		index: {
-			unique: true
-		}
-	},
-	client_secret: {
-		type: String,
-		index: {
-			unique: true
-		}
-	}
+  id: ObjectId,
+  createdat: {
+    type: Date,
+    'default': Date.now
+  },
+  updatedat: {
+    type: Date,
+    'default': Date.now
+  },
+  name: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  title: String,
+  user_id: {
+    type: ObjectId,
+    ref: 'User'
+  },
+  user_entity_type: {
+    type: String,
+    'default': 'user'
+  },
+  client_id: {
+    type: String,
+    index: {
+      unique: true
+    }
+  },
+  client_secret: {
+    type: String,
+    index: {
+      unique: true
+    }
+  },
+  ip_addresses: {
+    type: String,
+    default: null
+  },
+  rate_limit: {
+    max: {
+      type: Number,
+      default: 0
+    },
+    delayMs: {
+      type: Number,
+      default: 0
+    }
+  }
 });
 
 clientSchema.pre('save', function (next, done) {
-	// let client_id;
-	// let client_secret;
-	let salt = crypto.randomBytes(16).toString('base64');
-		
-	if (!this.user_id) {
-		next(new Error('Invalid User Id'));
-	}
-	else if (!this.name) {
-		next(new Error('Invalid Name'));
-	}
-	else {
-		// console.log('clientSchema pre validation');
-		let crypto_client_id = () => {
-	    return new Promise((resolve,reject) => {
-	    	// console.log('this.user_id',this.user_id);
-	    crypto.pbkdf2(this.user_id+new Date(), salt, 10, 16, 'sha512', (err, key) => {
-	        if (err) {
-	            reject(err);
-	        }
-	        else{
-	            resolve(key.toString('hex'));
-	        }
-	        });
-	    });
-		};
-		let crypto_client_secret = () => {
-	    return new Promise((resolve,reject) => {
-	    crypto.pbkdf2(this.random+new Date(), salt, 10, 16, 'sha512', (err, key) => {
-	        if (err) {
-	            reject(err);
-	        }
-	        else{
-	            resolve(key.toString('hex'));
-	        }
-	        });
-	    });
-		};
-    Promise.all([crypto_client_id(),crypto_client_secret()])
-    .then((client_data)=>{
-    	console.log('client_data',client_data);
-    	this.client_id = client_data[0];
-    	this.client_secret = client_data[1];
-			next();
-    })
-    .catch(function(err){
-			next(err);
-    });
-	}
+  // let client_id;
+  // let client_secret;
+  let salt = crypto.randomBytes(16).toString('base64');
+
+  if (!this.user_id) {
+    next(new Error('Invalid User Id'));
+  }
+  else if (!this.name) {
+    next(new Error('Invalid Name'));
+  }
+  else {
+    // console.log('clientSchema pre validation');
+    let crypto_client_id = () => {
+      return new Promise((resolve, reject) => {
+        // console.log('this.user_id',this.user_id);
+        crypto.pbkdf2(this.user_id + new Date(), salt, 10, 16, 'sha512', (err, key) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(key.toString('hex'));
+          }
+        });
+      });
+    };
+    let crypto_client_secret = () => {
+      return new Promise((resolve, reject) => {
+        crypto.pbkdf2(this.random + new Date(), salt, 10, 16, 'sha512', (err, key) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve(key.toString('hex'));
+          }
+        });
+      });
+    };
+    Promise.all([crypto_client_id(), crypto_client_secret()])
+      .then((client_data) => {
+        console.log('client_data', client_data);
+        this.client_id = client_data[0];
+        this.client_secret = client_data[1];
+        next();
+      })
+      .catch(function (err) {
+        next(err);
+      });
+  }
 });
 
 // clientSchema.post('init', function (doc) {
