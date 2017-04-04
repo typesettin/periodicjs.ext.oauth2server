17ba563f59191a5d77a8c8bfcc6f231e44210720
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-04-03 21:50:11 -0400

    fixed clients long

--- router/index.js
+++ router/index.js
@@ -6,15 +6,15 @@ module.exports = function(periodic) {
   const ExtensionRouter = periodic.express.Router();
   const oauth2serverRouter = require(path.resolve(__dirname, './oauth2server'))(periodic);
   const reUtilPath = path.join(__dirname, '../../../node_modules/periodicjs.ext.reactadmin/utility/locals.js');
-  let reactadmin = {manifest_prefix:'/r-admin'};
+  let reactadmin = { manifest_prefix: '/r-admin' };
 
   for (var x in periodic.settings.extconf.extensions) {
-		if (periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.reactadmin') {
+    if (periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.reactadmin') {
       const reactadminUtil = require(reUtilPath)(periodic).app.locals.extension.reactadmin;
       reactadmin = reactadminUtil;
-		}
-	}
-  ExtensionRouter.use(`${reactadmin.route_prefix}/extension/oauth2server`, oauth2serverRouter);
+    }
+  }
+  ExtensionRouter.use(`${reactadmin.manifest_prefix}extension/oauth2server`, oauth2serverRouter);
 
   return ExtensionRouter;
 };
