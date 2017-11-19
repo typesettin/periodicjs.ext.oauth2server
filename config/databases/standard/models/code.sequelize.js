4415dcb8189faaf70db5010b7573866b8d55526a
dany92 <dany@bc.edu>
2017-11-19 16:29:31 -0500

    added back association property on model schema and removed user_id field from client

--- config/databases/standard/models/code.sequelize.js
+++ config/databases/standard/models/code.sequelize.js
@@ -1,44 +1,44 @@
 'use strict';
 const Sequelize = require('sequelize');
 const scheme = {
-  _id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  value: {
-    type: Sequelize.INTEGER,
-    required: true,
-  },
-  user_id: {
-    type: Sequelize.INTEGER,
-    // type: ObjectId,
-    // ref: 'User'
-  },
-  user_username: {
-    type: Sequelize.INTEGER,
-  },
-  user_email: {
-    type: Sequelize.INTEGER,
-  },
-  user_entity_type: {
-    type: Sequelize.INTEGER,
-    'default': 'user',
-  },
-  redirect_uri: {
-    type: Sequelize.INTEGER,
-    required: true,
-  },
-  client_id: {
-    type: Sequelize.INTEGER,
-  },
+    _id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    value: {
+        type: Sequelize.INTEGER,
+        required: true,
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        // type: ObjectId,
+        // ref: 'User'
+    },
+    user_username: {
+        type: Sequelize.INTEGER,
+    },
+    user_email: {
+        type: Sequelize.INTEGER,
+    },
+    user_entity_type: {
+        type: Sequelize.INTEGER,
+        'default': 'user',
+    },
+    redirect_uri: {
+        type: Sequelize.INTEGER,
+        required: true,
+    },
+    client_id: {
+        type: Sequelize.INTEGER,
+    },
 };
 const options = {
-  underscored: true,
-  timestamps: true,
-  indexes: [{
-    fields: ['createdat', ],
-  }, ],
+    underscored: true,
+    timestamps: true,
+    indexes: [{
+        fields: ['createdat', ],
+    }, ],
 };
 // const associations = [{
 //   source: 'code',
@@ -50,13 +50,13 @@ const options = {
 // }, ];
 
 module.exports = {
-  scheme,
-  options,
-  // associations,
-  coreDataOptions: {
-    docid: ['_id', 'client_id', ],
-    sort: { createdat: -1, },
-    search: ['user_email', 'value', 'client_id', 'redirect_uri'],
-    // population: 'user_id',
-  },
+    scheme,
+    options,
+    associations: [],
+    coreDataOptions: {
+        docid: ['_id', 'client_id', ],
+        sort: { createdat: -1, },
+        search: ['user_email', 'value', 'client_id', 'redirect_uri'],
+        // population: 'user_id',
+    },
 };
